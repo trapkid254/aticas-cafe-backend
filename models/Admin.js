dc98071c9ccfecc3f2b155eb7059f3b94111b4a9
Delvin Opwoko <aleyladely5@gmail.com>
2025-10-13 22:23:41 +0300

    jhgdty

--- models/Admin.js
+++ models/Admin.js
@@ -5,7 +5,8 @@ const adminSchema = new Schema({
   employmentNumber: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
-  role: { type: String, default: 'admin' }
+  role: { type: String, default: 'admin' },
+  adminType: { type: String, enum: ['cafeteria', 'butchery'], required: true }
 });
 
-module.exports = mongoose.model('Admin', adminSchema); 
+module.exports = mongoose.model('Admin', adminSchema);
